6c40ed2f342a34423c4c3e40578128c2e4e91ee1
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-11-24 10:38:58 +0200

    initial commit backup

---
+++ week2/utils/imageMeta.js
@@ -0,0 +1,34 @@
+'use strict';
+const ExifImage = require('exif').ExifImage;
+
+const getCoordinates = (imgFile) => { // imgFile = full path to uploaded image
+  return new Promise((resolve, reject) => {
+    try {
+      const coordinates = [];
+        new ExifImage({image: 'imgFile'}, function(error, exifData) {
+          if (error) {
+            reject(error);
+          } else {
+            coordinates.push(gpsToDecimal(exifData.gps.GPSLongitude, exifData.gps.GPSLongitudeRef));
+            coordinates.push(gpsToDecimal(exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef));
+            resolve(coordinates);
+          }
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+// convert GPS coordinates to decimal format
+// for longitude, send exifData.gps.GPSLongitude, exifData.gps.GPSLongitudeRef
+// for latitude, send exifData.gps.GPSLatitude, exifData.gps.GPSLatitudeRef
+const gpsToDecimal = (gpsData, hem) => {
+  let d = parseFloat(gpsData[0]) + parseFloat(gpsData[1] / 60) +
+      parseFloat(gpsData[2] / 3600);
+  return (hem === 'S' || hem === 'W') ? d *= -1 : d;
+};
+
+module.exports = {
+  getCoordinates,
+};
